ad43a863865b4d548709024fcd0247427af28208
Fertan15 <tanferlinda@gmail.com>
2025-09-08 13:09:55 +0700

    init

---
+++ Tugas_1/Tugas_1/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Tugas_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.emailTbox = new System.Windows.Forms.TextBox();
+            this.usernameTbox = new System.Windows.Forms.TextBox();
+            this.fullnameTbox = new System.Windows.Forms.TextBox();
+            this.passwordTbox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.confirmpasswordTbox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.signupButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(283, 246);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(242, 299);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(235, 363);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Full Name";
+            // 
+            // emailTbox
+            // 
+            this.emailTbox.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailTbox.Location = new System.Drawing.Point(363, 239);
+            this.emailTbox.Name = "emailTbox";
+            this.emailTbox.Size = new System.Drawing.Size(285, 36);
+            this.emailTbox.TabIndex = 3;
+            this.emailTbox.TextChanged += new System.EventHandler(this.emailTbox_TextChanged);
+            // 
+            // usernameTbox
+            // 
+            this.usernameTbox.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameTbox.Location = new System.Drawing.Point(363, 297);
+            this.usernameTbox.Name = "usernameTbox";
+            this.usernameTbox.Size = new System.Drawing.Size(285, 36);
+            this.usernameTbox.TabIndex = 4;
+            this.usernameTbox.TextChanged += new System.EventHandler(this.usernameTbox_TextChanged);
+            // 
+            // fullnameTbox
+            // 
+            this.fullnameTbox.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fullnameTbox.Location = new System.Drawing.Point(363, 356);
+            this.fullnameTbox.Name = "fullnameTbox";
+            this.fullnameTbox.Size = new System.Drawing.Size(285, 36);
+            this.fullnameTbox.TabIndex = 5;
+            this.fullnameTbox.TextChanged += new System.EventHandler(this.fullnameTbox_TextChanged);
+            // 
+            // passwordTbox
+            // 
+            this.passwordTbox.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTbox.Location = new System.Drawing.Point(363, 417);
+            this.passwordTbox.Name = "passwordTbox";
+            this.passwordTbox.Size = new System.Drawing.Size(285, 36);
+            this.passwordTbox.TabIndex = 7;
+            this.passwordTbox.TextChanged += new System.EventHandler(this.passwordTbox_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(242, 424);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Password";
+            // 
+            // confirmpasswordTbox
+            // 
+            this.confirmpasswordTbox.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.confirmpasswordTbox.Location = new System.Drawing.Point(363, 481);
+            this.confirmpasswordTbox.Name = "confirmpasswordTbox";
+            this.confirmpasswordTbox.Size = new System.Drawing.Size(285, 36);
+            this.confirmpasswordTbox.TabIndex = 9;
+            this.confirmpasswordTbox.TextChanged += new System.EventHandler(this.confirmpasswordTbox_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(146, 483);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(214, 29);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Confirm Password";
+            // 
+            // signupButton
+            // 
+            this.signupButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.signupButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.signupButton.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signupButton.Location = new System.Drawing.Point(135, 579);
+            this.signupButton.Name = "signupButton";
+            this.signupButton.Size = new System.Drawing.Size(521, 50);
+            this.signupButton.TabIndex = 10;
+            this.signupButton.Text = "SIGN UP";
+            this.signupButton.UseVisualStyleBackColor = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.progressBar1.Location = new System.Drawing.Point(12, 34);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(842, 49);
+            this.progressBar1.TabIndex = 11;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 30F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(278, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(282, 57);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "REGISTER";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 688);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.signupButton);
+            this.Controls.Add(this.confirmpasswordTbox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.passwordTbox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.fullnameTbox);
+            this.Controls.Add(this.usernameTbox);
+            this.Controls.Add(this.emailTbox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox emailTbox;
+        private System.Windows.Forms.TextBox usernameTbox;
+        private System.Windows.Forms.TextBox fullnameTbox;
+        private System.Windows.Forms.TextBox passwordTbox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox confirmpasswordTbox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button signupButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Tugas_1/Tugas_1/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tugas_1
+{
+    public partial class Form1 : Form
+    {
+
+        bool emailAdded = false;
+        bool usernameAdded = false;
+        bool fullnameAdded = false;
+        bool passwordAdded = false;
+        bool confirmPasswordAdded = false;
+
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void emailTbox_TextChanged(object sender, EventArgs e)
+        {
+            //email
+            if (emailTbox.Text.Length == 0 && emailAdded == true && progressBar1.Value >= 20) { 
+                progressBar1.Value -= 20;
+                emailAdded = false;
+
+            }else if (emailTbox.Text.Length > 0 && emailAdded == false && progressBar1.Value <= 100) {
+                progressBar1.Value += 20;
+                emailAdded = true;
+
+            }
+
+        }
+
+        private void usernameTbox_TextChanged(object sender, EventArgs e)
+        {
+            //username
+            if (usernameTbox.Text.Length == 0 && usernameAdded == true && progressBar1.Value >= 20)
+            {
+                progressBar1.Value -= 20;
+                usernameAdded = false;
+
+            }
+            else if (usernameTbox.Text.Length > 0 && usernameAdded == false && progressBar1.Value <= 100)
+            {
+                progressBar1.Value += 20;
+                usernameAdded = true;
+
+            }
+
+        }
+
+        private void fullnameTbox_TextChanged(object sender, EventArgs e)
+        {
+            //username
+            if (fullnameTbox.Text.Length == 0 && fullnameAdded == true && progressBar1.Value >= 20)
+            {
+                progressBar1.Value -= 20;
+                fullnameAdded = false;
+
+            }
+            else if (fullnameTbox.Text.Length > 0 && fullnameAdded == false && progressBar1.Value <= 100)
+            {
+                progressBar1.Value += 20;
+                fullnameAdded = true;
+
+            }
+        }
+
+        private void passwordTbox_TextChanged(object sender, EventArgs e)
+        {
+            //username
+            if (passwordTbox.Text.Length == 0 && passwordAdded == true && progressBar1.Value >= 20)
+            {
+                progressBar1.Value -= 20;
+                passwordAdded = false;
+
+            }
+            else if (passwordTbox.Text.Length > 0 && passwordAdded == false && progressBar1.Value <= 100)
+            {
+                progressBar1.Value += 20;
+                passwordAdded = true;
+
+            }
+        }
+
+        private void confirmpasswordTbox_TextChanged(object sender, EventArgs e)
+        {
+            
+            if (confirmpasswordTbox.Text != passwordTbox.Text && confirmPasswordAdded == true)
+            {
+                progressBar1.Value -= 20;
+                confirmPasswordAdded = false;
+
+            }
+            else if (confirmpasswordTbox.Text == passwordTbox.Text && confirmPasswordAdded == false)
+            {
+                progressBar1.Value += 20;
+                confirmPasswordAdded = true;
+
+            }
+        }
+    }
+}
